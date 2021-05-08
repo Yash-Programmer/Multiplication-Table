8c348aa3a8274160f8b81b725f14e436506099c7
Yash Varshney <yash.gurukul123@gmail.com>
2021-05-08 22:10:30 +0530

    Add files via upload

---
+++ Multiplication Table.py
@@ -0,0 +1,7 @@
+Number = float(input("Enter The Number - "))
+for circle in range(1, 11):
+    print(Number, 'x', circle, '=', Number * circle)
+
+Statement = "This is the Table of"
+print("\n")
+print(Statement, Number)
